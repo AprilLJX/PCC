847b92d158b53d7ba9110e7f14c855c318243a6d
Jacyn <jacyn0421@gmail.com>
2019-12-19 20:12:03 +0800

    user相关逻辑实现

--- server/model/userModel.py
+++ server/model/userModel.py
@@ -2,25 +2,95 @@ import sqlite3
 
 class userModel:
 
+    #用户注册
     def signup_model(self,phone,password):
-
+        #连接数据库
         conn = sqlite3.connect("data/pccDB.db")
         c = conn.cursor()
 
+        #执行SQL语句
         SQL = "SELECT userid from usertable where phone = '%s' " % phone
         user = c.execute(SQL)
 
+        #判断phone是否存在
         user = list(user)
         phone_exist = True if len(user) else False
 
         res = {}
+        #phone存在无法注册，返回0
+        #phone不存在可注册，注册完毕返回1
         if phone_exist:
             res["status"] = '0'
         else:
+            #执行插入语句，记得commit
             c.execute("INSERT INTO usertable(phone,password) VALUES (?,?)", (phone, password))
             conn.commit()
             res["status"] = '1'
+        conn.close()
+
+        return res
+
+    #用户登录
+    def login_model(self,phone,password):
+        #连接数据库
+        conn = sqlite3.connect("data/pccDB.db")
+        c = conn.cursor()
+        cursor = c.execute("SELECT userid,phone,password from usertable where phone = '{}'".format(phone))
+
+        cursor = list(cursor)
+
+        res = {}
+        #TODO 在view层判断数据格式
+        if len(cursor):
+            #手机号密码匹配成功，登录返回uid和状态
+            if str(cursor[0][1]) == phone and str(cursor[0][2]) == password:
+                res['status'] = '1'
+                res['msg'] = '登录成功！'
+                res['uid'] = cursor[0][0]
+            else:
+                res['status'] = '0'
+                res['msg'] = '账号或密码错误，请重新登录！'
+        else:
+            res['status'] = '0'
 
         conn.close()
+
+        return res
+
+    #显示用户信息
+    def showUserMes_model(self,userid):
+
+        conn = sqlite3.connect("data/pccDB.db")
+        c = conn.cursor()
+        cursor = c.execute("SELECT * FROM usertable WHERE userid = {}".format(userid))
+        cursor = list(cursor)
+
+        res = {}
+
+        for row in cursor:
+            res["userid"] = row[0]
+            res["nickname"] = row[1]
+            res["phone"] = row[2]
+            res["headImg"] = row[3]
+            res["gender"] = row[4]
+            res["infor"] = row[5]
+
+        return res
+
+    #修改用户信息
+    def updateUser_model(self,nickname, password, headImg, gender, infor, userid):
+
+        conn = sqlite3.connect("data/pccDB.db")
+        c = conn.cursor()
+        # TODO 为什么这里要加引号
+        c.execute(
+            "UPDATE usertable SET nickname = '{}',password = '{}',headImg = '{}',gender = '{}',infor = '{}' WHERE userid = {}".format(
+                nickname, password, headImg, gender, infor, userid))
+
+        conn.commit()
+        conn.close()
+
+        res = {}
+        res["status"] = 1
 
         return res
--- server/view/userView.py
+++ server/view/userView.py
@@ -3,21 +3,67 @@ from flask import request,Blueprint
 from model import userModel
 
 user_view = Blueprint('user_view',__name__)
+user_model = userModel.userModel
 
-
-
+# phone:用户手机注册
+# passwoerd：密码
+#TODO:密码hash存储，手机号格式验证
 @user_view.route('/signup', methods=["GET", "POST"])
 def signup():
-    # data1 = request.get_json(force=True)
-    # data = ()
-    # print(type(data))
-
+    #获取客户端json
     data = json.loads(request.get_data(as_text=True))
     phone = data.get('phone')
     password = data.get('password')
 
-    user_model = userModel.userModel
+    #model层进行数据处理
     res = json.dumps(user_model.signup_model(user_model,phone,password))
 
     return res
-    # return "singup"
+
+# phone:用户手机注册
+# passwoerd：密码
+#TODO:手机号格式验证，多次登录验证，简单的安全登录
+@user_view.route('/login', methods=["GET", "POST"])
+def login():
+    # 获取客户端json
+    data = json.loads(request.get_data(as_text=True))
+    phone = data.get('phone')
+    password = data.get('password')
+
+    res = json.dumps(user_model.login_model(user_model,phone,password))
+    return res
+
+#显示用户信息
+#userid:用户id
+@user_view.route('/showusermessage', methods=["GET", "POST"])
+def showUserMes():
+    data = json.loads(request.get_data(as_text=True))
+    userid = data.get("userid")
+
+    res = json.dumps(user_model.showUserMes_model(userid))
+    return res
+
+#修改用户信息
+#userid: 用户id
+#nickname: 用户昵称
+#password: 用户密码
+#headImg: 用户头像
+#gender： 性别
+#infor：个性签名
+#todo 头像
+#TODO 修改密码，输入原密码，修改密码之后重新输入确认
+@user_view.route('/updateuser', methods=["GET", "POST"])
+def updateUser():
+    data = json.loads(request.get_data(as_text=True))
+    userid = data.get("userid")
+    nickname = data.get("nickname")
+    password = data.get("password")
+    headImg = data.get("headImg")
+    gender = data.get("gender")
+    infor = data.get("infor")
+
+
+    res = user_model.updateUser_model(user_model,nickname,password,headImg,gender,infor,userid)
+
+    return res
+
